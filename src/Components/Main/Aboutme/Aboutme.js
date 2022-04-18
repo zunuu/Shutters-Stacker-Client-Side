55fc57131bed1b77c55799eb5ea12fc25a6caef8
zunuu <zunayedhossain99@gmail.com>
2022-04-18 07:19:29 +0600

    about-udated

--- src/Components/Main/Aboutme/Aboutme.js
+++ src/Components/Main/Aboutme/Aboutme.js
@@ -3,10 +3,10 @@ import img from '../../../Images/aboutme.jpg'
 import './Aboutme.css'
 const Aboutme = () => {
     return (
-        <div className='d-flex p-5 blogs '>
+        <div className='d-flex p-5 blogs justify-content-center align-items-center'>
             <div className='col-xl-6 '>
                 <h2 className='greetings'>
-                    Assalamu Alaikum, It's Zunayed Hossain
+                    Assalamu Alaikum, It's <span className='name'>Zunayed Hossain</span>
                 </h2>
                 <p>A photography enthusiast.I like mostly wildlife photography.My objective is to spread the thoughts in lens which elaborate a the defition of perfect story telling.There's lots of story around us some of them got our attention some of them become ignored.I try try to capture each moment especially in wildlife & zoogeography,I want to explore more ,want to capture more moments of the nature,want to learn more about Photography.Thus Photography is my Enthusiasm.I will work,explore more in the future</p>
             </div>
